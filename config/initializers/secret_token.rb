3182c00ba701bb4b76b3545092bfc5166673ec54
Joshua Chestang <joshua.chestang@gmail.com>
2013-11-28 22:37:32 -0600

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+IllinoisSexualHealth::Application.config.secret_key_base = '11f46d8726100d05a3813d7aa5f9f835f94033bad43e2d88a2b48b4ce2d33641dac7cff86c1d1e46e59aa57e93fb6d6d3c20b6e559cc70a4a1cf6a7d65723d75'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+IllinoisSexualHealth::Application.config.session_store :cookie_store, key: '_illinois_sexual_health_session'
